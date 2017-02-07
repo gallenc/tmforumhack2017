77a4c9352fb1d2c47ac447c4806d3e0e1764a306
cgallen <cgallen@opennms.org>
2017-02-07 13:08:25 +0000

    working ri :)

--- tmforum-rest-spm/tmforum-rest-spm/tmforum-rest-spm-ri/main-module/src/main/java/org/openoss/karaf/features/tmforum/spm/ri/ServiceProblemServiceImpl.java
+++ tmforum-rest-spm/tmforum-rest-spm/tmforum-rest-spm-ri/main-module/src/main/java/org/openoss/karaf/features/tmforum/spm/ri/ServiceProblemServiceImpl.java
@@ -1,7 +1,12 @@
 package org.openoss.karaf.features.tmforum.spm.ri;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response.Status;
@@ -26,14 +31,27 @@ import org.slf4j.LoggerFactory;
 
 public class ServiceProblemServiceImpl implements ServiceProblemService {
 	private static final Logger LOG = LoggerFactory.getLogger(ServiceProblemServiceImpl.class);
+	
+	AtomicInteger serviceProblemId = new AtomicInteger(0);
+		
+	SortedMap<String, ServiceProblem> serviceProblemList = Collections.synchronizedSortedMap(new TreeMap<String,ServiceProblem>());
 
 	@Override
-	public ServiceProblemResponse getServiceProblem(String id,	List<String> fields) {
+	public synchronized ServiceProblemResponse getServiceProblem(String id,	List<String> fields) {
 		LOG.debug("getServiceProblem called for id="+id);
-		ServiceProblem problem = new ServiceProblem();
-		problem.setId(id);
-		problem.setHref("serviceProblem/"+id);
+//		ServiceProblem problem = new ServiceProblem();
+//		problem.setId(id);
+//		problem.setHref("serviceProblem/"+id);
 		ServiceProblemResponse serviceProblemResponse=new ServiceProblemResponse();
+		
+		ServiceProblem problem = serviceProblemList.get(id);
+		if(problem==null){
+			StatusMessage statusMessage= new StatusMessage(Status.NOT_FOUND);
+			statusMessage.setDeveloperMessage("ServiceProblem not found with id="+id);
+			serviceProblemResponse.setStatusMessage(statusMessage);
+			LOG.debug("getServiceProblem response status="+statusMessage.getStatus());
+			return serviceProblemResponse;
+		}
 		serviceProblemResponse.setProblem(problem);
 
 		StatusMessage statusMessage= new StatusMessage(Status.OK);
@@ -45,21 +63,22 @@ public class ServiceProblemServiceImpl implements ServiceProblemService {
 	}
 
 	@Override
-	public ServiceProblemsResponse getServiceProblems(
+	public synchronized  ServiceProblemsResponse getServiceProblems(
 			MultivaluedMap<String, String> queryParams, List<String> fields, Integer lowRange, Integer hiRange) {
 		LOG.debug("getServiceProblems implementation called");
 
-		ServiceProblem sp = new ServiceProblem();
-		sp.setId(Integer.toString(10));
-		sp.setHref("serviceProblem/"+10);
-		ServiceProblem sp2 = new ServiceProblem();
-		sp2.setId(Integer.toString(11));
-		sp2.setHref("serviceProblem/"+11);
-
-		List<ServiceProblem> splist= new ArrayList<ServiceProblem>();
-		splist.add(sp);
-		splist.add(sp2);
-
+//		ServiceProblem sp = new ServiceProblem();
+//		sp.setId(Integer.toString(10));
+//		sp.setHref("serviceProblem/"+10);
+//		ServiceProblem sp2 = new ServiceProblem();
+//		sp2.setId(Integer.toString(11));
+//		sp2.setHref("serviceProblem/"+11);
+//
+//		List<ServiceProblem> splist= new ArrayList<ServiceProblem>();
+//		splist.add(sp);
+//		splist.add(sp2);
+		
+		List<ServiceProblem> splist = new ArrayList<ServiceProblem>( serviceProblemList.values());
 
 		ServiceProblemsResponse serviceProblemsResponse= new ServiceProblemsResponse();
 		serviceProblemsResponse.setProblems(splist);
@@ -71,8 +90,20 @@ public class ServiceProblemServiceImpl implements ServiceProblemService {
 	}
 
 	@Override
-	public ServiceProblemResponse postServiceProblem(ServiceProblem serviceProblem) {
+	public synchronized  ServiceProblemResponse postServiceProblem(ServiceProblem serviceProblem) {
 		LOG.debug("postServiceProblem called for serviceproblem="+serviceProblem);
+		
+		String id =  Integer.toString(serviceProblemId.addAndGet(1));
+		serviceProblem.setId(id);
+		
+		String href= "http://localhost:8181/serviceProblemManagement/rest/v1-0/spm/api/serviceProblem/"+id;
+		serviceProblem.setHref(href);
+		
+		if(serviceProblem.getTimeRaised()==null){
+			serviceProblem.setTimeRaised(new Date());
+		}
+		
+		serviceProblemList.put(id, serviceProblem);
 		
 		ServiceProblemResponse serviceProblemResponse=new ServiceProblemResponse();
 		serviceProblemResponse.setProblem(serviceProblem);
@@ -84,10 +115,28 @@ public class ServiceProblemServiceImpl implements ServiceProblemService {
 	}
 
 	@Override
-	public ServiceProblemResponse putServiceProblem(String id, ServiceProblem serviceProblem) {
+	public synchronized  ServiceProblemResponse putServiceProblem(String id, ServiceProblem serviceProblem) {
 		LOG.debug("putServiceProblem called for id="+id+" serviceproblem="+serviceProblem);
 		
 		ServiceProblemResponse serviceProblemResponse=new ServiceProblemResponse();
+		ServiceProblem problem = serviceProblemList.get(id);
+		if(problem==null){
+			StatusMessage statusMessage= new StatusMessage(Status.NOT_FOUND);
+			statusMessage.setDeveloperMessage("ServiceProblem not found with id="+id);
+			serviceProblemResponse.setStatusMessage(statusMessage);
+			LOG.debug("putServiceProblem response status="+statusMessage.getStatus());
+			return serviceProblemResponse;
+		}
+		
+		serviceProblem.setId(id); //TODO
+		
+		if(serviceProblem.getTimechanged()==null){
+			serviceProblem.setTimechanged(new Date());
+		}
+		
+		serviceProblemList.put(id, serviceProblem);
+		
+
 		serviceProblemResponse.setProblem(serviceProblem);
 
 		StatusMessage statusMessage= new StatusMessage(Status.OK);
@@ -97,10 +146,27 @@ public class ServiceProblemServiceImpl implements ServiceProblemService {
 	}
 	
 	@Override
-	public ServiceProblemResponse patchServiceProblem(String id, ServiceProblem serviceProblem) {
+	public synchronized  ServiceProblemResponse patchServiceProblem(String id, ServiceProblem serviceProblem) {
 		LOG.debug("patchServiceProblem called for id="+id+" serviceproblem="+serviceProblem);
 		
 		ServiceProblemResponse serviceProblemResponse=new ServiceProblemResponse();
+		ServiceProblem problem = serviceProblemList.get(id);
+		if(problem==null){
+			StatusMessage statusMessage= new StatusMessage(Status.NOT_FOUND);
+			statusMessage.setDeveloperMessage("ServiceProblem not found with id="+id);
+			serviceProblemResponse.setStatusMessage(statusMessage);
+			LOG.debug("putServiceProblem response status="+statusMessage.getStatus());
+			return serviceProblemResponse;
+		}
+		
+		serviceProblem.setId(id); //TODO
+		
+		if(serviceProblem.getTimechanged()==null){
+			serviceProblem.setTimechanged(new Date());
+		}
+		
+		serviceProblemList.put(id, serviceProblem); //todo patch
+		
 		serviceProblemResponse.setProblem(serviceProblem);
 
 		StatusMessage statusMessage= new StatusMessage(Status.OK);
@@ -110,14 +176,30 @@ public class ServiceProblemServiceImpl implements ServiceProblemService {
 	}
 
 	@Override
-	public Reply deleteServiceProblem(String id) {
+	public synchronized  Reply deleteServiceProblem(String id) {
 		LOG.debug("deleteServiceProblem called for id="+id);
-		// TODO Auto-generated method stub
-		throw new  RuntimeException("deleteServiceProblem not implimented");
+		
+		ServiceProblem problem = serviceProblemList.remove(id);
+
+		ServiceProblemResponse serviceProblemResponse=new ServiceProblemResponse();
+
+		if(problem==null){
+			StatusMessage statusMessage= new StatusMessage(Status.NOT_FOUND);
+			statusMessage.setDeveloperMessage("ServiceProblem could not be deleted (not found) with id="+id);
+			serviceProblemResponse.setStatusMessage(statusMessage);
+			LOG.debug("deleteServiceProblem response status="+statusMessage.getStatus());
+			return serviceProblemResponse;
+		}
+		
+		StatusMessage statusMessage= new StatusMessage(Status.OK);
+		LOG.debug("deleteServiceProblem response status="+statusMessage.getStatus());
+		serviceProblemResponse.setStatusMessage(statusMessage);
+		return serviceProblemResponse;
+
 	}
 
 	@Override
-	public ServiceProblemAckResponse ackServiceProblem(
+	public synchronized  ServiceProblemAckResponse ackServiceProblem(
 			ServiceProblemAckRequest serviceProblemAckRequest) {
 		LOG.debug("ackServiceProblem called");
 		// TODO Auto-generated method stub
@@ -125,7 +207,7 @@ public class ServiceProblemServiceImpl implements ServiceProblemService {
 	}
 
 	@Override
-	public ServiceProblemUnAckResponse unackServiceProblem(
+	public synchronized  ServiceProblemUnAckResponse unackServiceProblem(
 			ServiceProblemUnAckRequest serviceProblemUnAckRequest) {
 		LOG.debug("unackServiceProblems called");
 		// TODO Auto-generated method stub
@@ -133,7 +215,7 @@ public class ServiceProblemServiceImpl implements ServiceProblemService {
 	}
 
 	@Override
-	public ServiceProblemGroupResponse groupServiceProblem(
+	public synchronized  ServiceProblemGroupResponse groupServiceProblem(
 			ServiceProblemGroupRequest groupRequest) {
 		LOG.debug("groupServiceProblem called");
 		// TODO
@@ -141,7 +223,7 @@ public class ServiceProblemServiceImpl implements ServiceProblemService {
 	}
 
 	@Override
-	public ServiceProblemUngroupResponse ungroupServiceProblem(
+	public synchronized  ServiceProblemUngroupResponse ungroupServiceProblem(
 			ServiceProblemUngroupRequest unGroupRequest) {
 		LOG.debug("ungroupServiceProblems called");
 		// TODO Auto-generated method stub
@@ -149,7 +231,7 @@ public class ServiceProblemServiceImpl implements ServiceProblemService {
 	}
 
 	@Override
-	public ServiceProblemEventRecordsResponse getServiceProblemEventRecords(
+	public synchronized  ServiceProblemEventRecordsResponse getServiceProblemEventRecords(
 			MultivaluedMap<String, String> queryParams, List<String> fields,
 			Integer lowRange, Integer hiRange) {
 		LOG.debug("getServiceProblemEventRecords called");
@@ -158,7 +240,7 @@ public class ServiceProblemServiceImpl implements ServiceProblemService {
 	}
 
 	@Override
-	public ServiceProblemEventRecordsResponse getServiceProblemEventRecord(
+	public synchronized  ServiceProblemEventRecordsResponse getServiceProblemEventRecord(
 			String id) {
 		LOG.debug("getServiceProblemEventRecord called for id="+id);
 		// TODO Auto-generated method stub
