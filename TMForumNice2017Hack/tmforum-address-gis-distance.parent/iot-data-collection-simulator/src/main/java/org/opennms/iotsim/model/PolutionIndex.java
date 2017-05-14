456edb1828586dd84ff696ce97d8c8d9a2e1bd84
Craig Gallen <cgallen@opennms.org>
2017-05-14 07:59:20 +0100

    changes to iotsim

--- TMForumNice2017Hack/tmforum-address-gis-distance.parent/iot-data-collection-simulator/src/main/java/org/opennms/iotsim/model/PolutionIndex.java
+++ TMForumNice2017Hack/tmforum-address-gis-distance.parent/iot-data-collection-simulator/src/main/java/org/opennms/iotsim/model/PolutionIndex.java
@@ -10,22 +10,40 @@ import java.util.List;
  */
 public class PolutionIndex {
 
-	
+	//public static final String Low 	
+
+	String getPolutionBanding(int index){
+		if ( index <1 || index > 10) throw new IllegalArgumentException("polution banding must be in range 1-10 not "+index);
+		String polutionBanding=null;
+		switch (index) {
+		case 1: case 2: case 3:  polutionBanding = "Low"; // low 1-3
+		break;
+		case 4: case 5:case 6:  polutionBanding = "Moderate"; // Moderate 	4–6
+		break;
+		case 7: case 8: case 9:  polutionBanding = "Moderate"; // High 	7–9
+		break;
+		case 10:  polutionBanding = "VeryHigh"; // Very High 10
+		break;
+		}
+		return polutionBanding;
+	}
+
+
 	public List<NameValuePair> getParameters(){
 		List<NameValuePair> parameters = new ArrayList<NameValuePair>();
-		  // mobile parameters
-		parameters.add(new NameValuePair("ozone_μg_m3","100.00"));
-		parameters.add(new NameValuePair("nitrogenDioxide_μg_m3","100.00"));
-		parameters.add(new NameValuePair("SulphurDioxide_μg_m3","100.00"));
-		parameters.add(new NameValuePair("pM2.5Particles_μg_m3","100.00"));
-		parameters.add(new NameValuePair("pM10Particles_μg_m3","100.00"));
-		
+		// mobile parameters
+		parameters.add(new NameValuePair("ozone_μg_m3","241")); // 8 hr mean
+		parameters.add(new NameValuePair("nitrogenDioxide_μg_m3","601")); //1 hr mean
+		parameters.add(new NameValuePair("sulphurDioxide_μg_m3","1065")); // 15 min mean
+		parameters.add(new NameValuePair("pM2.5Particles_μg_m3","71")); // 24 hr mean
+		parameters.add(new NameValuePair("pM10Particles_μg_m3","101")); // 24 hr mean
+
 		// 1-10
-		parameters.add(new NameValuePair("estimated_UK_Polution_index","10"));
-		
-		parameters.add(new NameValuePair("estimated_UK_Air_pollution_banding","Severe"));
-		
+		parameters.add(new NameValuePair("estimated_UK_Polution_index","10")); //0-10
+
+		parameters.add(new NameValuePair("estimated_UK_Air_pollution_banding","Very_High"));
+
 		return parameters;
 	}
-	
+
 }
