029251a1d34093fa4ea8bc3acab931dd26e83a22
2sievm82 <2sievm82@solent.ac.uk>
2017-05-15 15:19:08 +0200

    Added some of the store front

--- TMForumNice2017Hack/private/jergus/src/controllers/StoreController.js
+++ TMForumNice2017Hack/private/jergus/src/controllers/StoreController.js
@@ -1,8 +1,16 @@
 export default function (SalesForceService) {
 
+    this.basket = [];
+
     SalesForceService.get_products().then((response) => {
-        console.log(response);
-        this.product = response.data;
-    })
+        this.products = response.data.map(product => {
+            product.category_name = product['category.name'];
+            return product;
+        });
+    });
+
+    this.add = function (item) {
+        this.basket.push(item);
+    };
 };
 
