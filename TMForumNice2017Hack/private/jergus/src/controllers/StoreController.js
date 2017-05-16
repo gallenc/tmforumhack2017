c187af64d13960f2b4b58bd105e20468d18176de
jerguslejko <jergus.lejko@gmail.com>
2017-05-16 13:33:48 +0200

    fix ordering

--- TMForumNice2017Hack/private/jergus/src/controllers/StoreController.js
+++ TMForumNice2017Hack/private/jergus/src/controllers/StoreController.js
@@ -1,4 +1,8 @@
 export default function ($timeout, HuaweiHTTPService, SalesForceService, $uibModal, ngToast) {
+    this.balance = 0;
+    HuaweiHTTPService.get_balance().then(response => {
+        this.balance = response.data[0].remainedAmount.amount;
+    })
 
     this.open = function (id) {
 
@@ -18,11 +22,7 @@ export default function ($timeout, HuaweiHTTPService, SalesForceService, $uibMod
 
         modalInstance.result.then( (selectedItem) => {
             this.charge(selectedItem.item.product, selectedItem.quantity, selectedItem.total);
-        }, function () {
-
         });
-
-
     }
 
 
@@ -40,10 +40,15 @@ export default function ($timeout, HuaweiHTTPService, SalesForceService, $uibMod
 
     this.charge = function ($product, $quantity, $amount) {
         $timeout(() => {
-            ngToast.create("Amount "+$amount+" Charged for "+$quantity+" of "+$product.name);
+            ngToast.create("Amount £"+ (parseFloat($amount).toFixed(2)) +" Charged for "+$quantity+" of "+$product.name);
         }, 500);
 
-        HuaweiHTTPService.charge_amount($amount);
+        HuaweiHTTPService.charge_amount($amount)
+            .then(() => {
+                HuaweiHTTPService.get_balance().then(response => {
+                    this.balance = response.data[0].remainedAmount.amount;
+                })
+            });
     }
 }
 
